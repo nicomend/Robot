6e677ec6a4d094c8233e2261859e2b7d42368d7d
dorperetz <sdorperetz@gmail.com>
2017-06-20 17:29:50 +0300

    Update HamsterAPIClientOpenCVImageExample.cpp

--- src/HamsterAPIClientOpenCVImageExample.cpp
+++ src/HamsterAPIClientOpenCVImageExample.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 using namespace HamsterAPI;
 
-
 int main() {
 	try {
 		Hamster* hamster = new HamsterAPI::Hamster(1);
@@ -11,43 +10,40 @@ int main() {
 		cv::namedWindow("OccupancyGrid-view");
 		while (hamster->isConnected()) {
 			try {
-				int resulation = 10;
+
 				int robotRadiusInCM = 20;
-				int pixels = robotRadiusInCM / resulation ;
+
 				OccupancyGrid ogrid = hamster->getSLAMMap();
 
+				int resulation = ogrid.getResolution() * 100;
+				int pixels = robotRadiusInCM / resulation;
 				int width = ogrid.getWidth();
 				int height = ogrid.getHeight();
 				unsigned char pixel;
-				cv::Mat m = cv::Mat(width, height,CV_8UC1);
+				cv::Mat m = cv::Mat(width, height, CV_8UC1);
 
 				// Build the pixel mat
 				for (int i = 0; i < height; i++)
 					for (int j = 0; j < width; j++) {
 						if (ogrid.getCell(i, j) == CELL_FREE)
 							pixel = 255;
-						else if (ogrid.getCell(i, j) == CELL_OCCUPIED)
+						else if (ogrid.getCell(i, j) == CELL_OCCUPIED) {
 							pixel = 0;
-						else
+						} else
 							pixel = 128;
-						m.at<unsigned char>(i,j) = pixel;
-						/**if (true){
-						for (int x=i-resulation; x < i+resulation;x++){
-							for (int y = j - resulation; y < j+resulation; y++){
-							m.at<unsigned char>(x,y) = pixel;
+						m.at<unsigned char>(i, j) = pixel;
+
+						if (pixel == 0) {
+							for (int x = i - pixels; x < i + pixels; x++) {
+								for (int y = j - pixels; y < j + pixels; y++) {
+									m.at<unsigned char>(x, y) = pixel;
+								}
 							}
-						}**/
-						if (pixel == 0){
-						for (int x=i-pixels; x < i+pixels;x++){
-							for (int y = j - pixels; y < j+pixels; y++){
-							m.at<unsigned char>(x,y) = pixel;
-							}
-						}
 						}
 					}
 
 				// Show the pixel mat
-				cv::imshow("OccupancyGrid-view",m);
+				cv::imshow("OccupancyGrid-view", m);
 				cv::waitKey(1);
 
 			} catch (const HamsterAPI::HamsterError & message_error) {
